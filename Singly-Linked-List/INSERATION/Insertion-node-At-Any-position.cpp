d7d93004b5d74d3a691577a7c40221d25742d1a9
Saad123ali <itmesaad@gmail.com>
2024-09-22 12:21:16 +0500

    update position-node-file

--- Singly-Linked-List/INSERATION/Insertion-node-At-Any-position.cpp
+++ Singly-Linked-List/INSERATION/Insertion-node-At-Any-position.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 class Node
 {
-    public:
+public:
     int val;
-    Node* next;
+    Node *next;
 
     Node(int data)
     {
@@ -16,20 +16,20 @@ class Node
 
 class LinkedList
 {
-    private:
-    Node* head;
-    public:
+private:
+    Node *head;
 
+public:
     LinkedList()
     {
         head = nullptr;
     }
 
-    void insertAtBeginning( int val)
+    void insertAtBeginning(int val)
     {
-        Node* new_node = new Node(val);
-        
-        if(head == nullptr)
+        Node *new_node = new Node(val);
+
+        if (head == nullptr)
         {
             head = new_node;
         }
@@ -42,14 +42,14 @@ class LinkedList
 
     void insertAtTail(int val)
     {
-        Node* new_node = new Node(val);
+        Node *new_node = new Node(val);
 
         if (head == nullptr)
         {
             head = new_node;
         }
 
-        Node* temp = head;
+        Node *temp = head;
         while (temp->next != nullptr)
         {
             temp = temp->next;
@@ -57,35 +57,40 @@ class LinkedList
         temp->next = new_node;
     }
 
-    void insertAtPosition(int val, int pos) {
-    // Handle inserting at the head (position 0)
-    if (pos == 0) {
-        insertAtBeginning(val);
-        return;  // Exit after inserting at the beginning
+    void insertAtPosition(int val, int pos)
+    {
+        // Handle inserting at the head (position 0)
+        if (pos == 0)
+        {
+            insertAtBeginning(val);
+            return; // Exit after inserting at the beginning
+        }
+
+        Node *new_node = new Node(val);
+        Node *temp = head;
+        int currPosition = 0; // Start at position 0
+
+        while (currPosition < pos - 1 && temp != nullptr)
+        {
+            temp = temp->next;
+            currPosition++;
+        }
+
+        if (temp == nullptr)
+        {
+            cout << "Position out of bounds!" << endl;
+            delete new_node; // Free memory if the position is invalid
+        }
+        else
+        {
+            new_node->next = temp->next;
+            temp->next = new_node;
+        }
     }
-
-    Node* new_node = new Node(val);
-    Node* temp = head;
-    int currPosition = 0;  // Start at position 0
-
-    while (currPosition < pos - 1 && temp != nullptr) {
-        temp = temp->next;
-        currPosition++;
-    }
-
-    if (temp == nullptr) {
-        cout << "Position out of bounds!" << endl;
-        delete new_node;  // Free memory if the position is invalid
-    } else {
-        new_node->next = temp->next;
-        temp->next = new_node;
-    }
-}
-
 
     void display()
     {
-        Node* temp = head;
+        Node *temp = head;
         while (temp != nullptr)
         {
             cout << temp->val << "->";
@@ -93,31 +98,29 @@ class LinkedList
         }
         cout << "NULL" << endl;
     }
-
 };
 
 int main()
 {
     LinkedList list;
 
-    list.insertAtBeginning(1);  //insert at beginning of list
+    list.insertAtBeginning(1); // insert at beginning of list
     list.display();
-    list.insertAtBeginning(2); //insert at beginning of list
+    list.insertAtBeginning(2); // insert at beginning of list
     list.display();
-    list.insertAtTail(4); //insert at tail of list
+    list.insertAtTail(4); // insert at tail of list
     list.display();
-    list.insertAtPosition(5, 2);  // Valid position
+    list.insertAtPosition(5, 2); // Valid position
     list.display();
-    list.insertAtPosition(6, 6);  // Invalid position
+    list.insertAtPosition(6, 6); // Invalid position
     list.display();
 
     return 0;
 }
 
-
-//Output of the code below
-// 1->NULL
-// 2->1->NULL
-// 2->1->4->NULL
-// 2->5->1->4->NULL       
-// Position out of bounds!
+// Output of the code below
+//  1->NULL
+//  2->1->NULL
+//  2->1->4->NULL
+//  2->5->1->4->NULL
+//  Position out of bounds!
